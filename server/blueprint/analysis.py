5a18bf4001dfb421c45015d394596e65a923ab8f
mr_wuliu <mr_wuliu@foxmail.com>
2024-02-21 09:56:01 +0800

    同步PC

--- server/blueprint/analysis.py
+++ server/blueprint/analysis.py
@@ -8,7 +8,7 @@ def home():
 
 @bp.route('/sample')
 def sample():
-    moves = [("b",(3,3)), ("w",(15,15))] #,("b",(16,16)),("w",(16,15)),("b",(15,16))]
+    moves = [("b",(3,3)), ("w",(15,15)), ("b",(16,16)),("w",(16,15)),("b",(15,16))]
     katago = KataGO(moves)
     text = katago.query()
     print(text)
